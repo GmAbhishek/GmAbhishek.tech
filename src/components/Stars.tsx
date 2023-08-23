c5d467c27ced6d4c14cc139e81c9deb8c87de973
Gm_Abhishek <gmabhishek2004@gmail.com>
2023-08-23 11:29:39 +0530

    Remove huge css

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -1,15 +1,10 @@
-import StaryNights from '../assets/'
-import '../css/stars.css'
+
 function Stars(): JSX.Element {
     
     return (
         <div className="background">
-             <video className="video-background" autoPlay loop muted>
-                <source src={StaryNights} type="video/webm" />
-                Your browser does not support the video tag.
-            </video>
         </div>
     )
 }
-
 export default Stars
+
